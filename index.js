ab1df541fcaaa5f6756e196f29cf606bc412bbb2
tiyawt <tiyaland3@gmail.com>
2025-10-23 13:00:54 +0700

    update app & index

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-import express from "express";
 import dotenv from "dotenv";
-import connectToDb from "./src/config/database.js";
-
 dotenv.config();
 
-const app = express();
+import connectToDb from "./src/config/database.js";
+import app from "./src/app.js";
+
 const PORT = process.env.PORT || 5000;
 
 try {
-  await connectToDb(); 
-  app.get("/", (req, res) => {
-    res.json({ message: "MongoDB connection success!" });
-  });
+  await connectToDb();
   app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`✅ Server running on http://localhost:${PORT}`);
   });
 } catch (err) {
-  console.error("Startup failed:", err?.message || err);
+  console.error("❌ Startup failed:", err?.message || err);
   process.exit(1);
 }
--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
-//app.js
+// src/app.js
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import routes from "./routes/routes.js";
+import passport from "./config/passport.js";
+
+const app = express();
+
+// Global middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
+app.use(passport.initialize());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Server up and running 🚀" });
+});
+
+// All API routes
+app.use("/api", routes);
+
+export default app;
