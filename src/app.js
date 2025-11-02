27124bd59ded1fcb4940cf3337300a18a5a49261
tiyawt <tiyaland3@gmail.com>
2025-11-02 16:01:43 +0700

    create user crud

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
-import cookieParser from "cookie-parser";    
+import cookieParser from "cookie-parser";
 import routes from "./routes/routes.js";
 import passport from "./config/passport.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
@@ -10,19 +10,20 @@ import gameRoutes from "./routes/gameRoutes.js";
 import ecoenzimRoutes from "./routes/ecoenzimRoutes.js";
 import cron from "node-cron";
 import { autoCancelExpiredProjects } from "./controllers/ecoenzimController.js";
-import "./jobs/sortingStreakReset.js";
-
+import userManagementRoutes from "./routes/userManagementRoutes.js";
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:3000", // FE 
-  credentials: true,               // untuk kirim/terima cookie
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000", // FE
+    credentials: true, // untuk kirim/terima cookie
+  })
+);
 
 // Global middleware
 app.use(express.json());
-app.use(cookieParser());  
+app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(passport.initialize());
 
@@ -35,13 +36,11 @@ app.use("/api", routes);
 app.use("/api/notifications", notificationRoutes);
 app.use("/api/game", gameRoutes);
 app.use("/api/ecoenzim", ecoenzimRoutes);
+app.use("/api/users", userManagementRoutes);
 
-
-
-cron.schedule('* * * * *', async () => {
-  console.log('⏳ Cek project kadaluarsa...');
+cron.schedule("* * * * *", async () => {
+  console.log("⏳ Cek project kadaluarsa...");
   await autoCancelExpiredProjects();
 });
-
 
 export default app;
